50ca1dc084473bf029a7d82cf44fb17a988d8854
93hong <sk2rldnr@gmail.com>
2020-05-27 19:26:06 +0900

    spring boot 2.3 setup

---
+++ src/test/java/com/example/spring5pro/Spring5proApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring5pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5proApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
